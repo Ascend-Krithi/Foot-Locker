e09d330b9d47737b2fdcf66eb8cf6ba286a246de
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-02-22 19:51:16 +0530

    Deploying Page Objects

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -1,11 +1,32 @@
-# HomePage.py
+# imports
+import json
+import os
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class HomePage:
-    def __init__(self, driver: WebDriver):
+    def __init__(self, driver):
         self.driver = driver
+        with open(os.path.join(os.path.dirname(__file__), '../Locators/Locators.json')) as f:
+            self.locators = json.load(f)
 
-    def click_find_store_link(self):
-        find_store_link = self.driver.find_element(By.XPATH, "//a[contains(@href, '/store-locator') and contains(text(), 'Find a Store')]")
-        find_store_link.click()
+    def load_homepage(self, url='https://www.footlocker.com/'):
+        self.driver.get(url)
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((
+                getattr(By, self.locators['homepage_find_store_link']['by'].upper()),
+                self.locators['homepage_find_store_link']['value']
+            ))
+        )
+        return True
+
+    def click_find_a_store(self):
+        find_store = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((
+                getattr(By, self.locators['homepage_find_store_link']['by'].upper()),
+                self.locators['homepage_find_store_link']['value']
+            ))
+        )
+        find_store.click()
+        return True
