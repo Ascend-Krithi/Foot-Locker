318c10e3a221eda553064a8d01cef94d156ed1a2
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-02-22 18:47:01 +0530

    Deploying Page Objects

--- Pages/StoreLocatorPage.py
+++ Pages/StoreLocatorPage.py
@@ -1,5 +1,6 @@
 # imports
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class StoreLocatorPage:
     def __init__(self, driver, locators):
@@ -26,3 +27,57 @@ class StoreLocatorPage:
 
     def verify_confirmation(self):
         return self.driver.find_element(By.CSS_SELECTOR, self.locators['confirmation_indicator']['value']).is_displayed()
+
+    def is_find_store_popup_displayed(self):
+        """
+        Verifies if the 'Find a Store' popup is displayed below the button.
+        Assumes the popup is anchored to the 'Select My Store' button.
+        """
+        try:
+            element = self.driver.find_element(By.XPATH, self.locators['select_my_store_button']['value'])
+            return element.is_displayed()
+        except NoSuchElementException:
+            return False
+
+    def get_find_store_popup_message(self):
+        """
+        Returns the popup message text for the 'Find a Store' popup.
+        Assumes the message is in a sibling or parent of the 'Select My Store' button.
+        """
+        try:
+            # This XPath assumes the message is in a parent div of the button
+            button = self.driver.find_element(By.XPATH, self.locators['select_my_store_button']['value'])
+            popup_container = button.find_element(By.XPATH, "./ancestor::div[1]")
+            return popup_container.text
+        except NoSuchElementException:
+            return ""
+
+    def is_select_my_store_link_visible(self):
+        """
+        Verifies the presence of the 'Select My Store' link/button within the popup.
+        """
+        try:
+            element = self.driver.find_element(By.XPATH, self.locators['select_my_store_button']['value'])
+            return element.is_displayed()
+        except NoSuchElementException:
+            return False
+
+    def is_location_textbox_present(self):
+        """
+        Verifies the presence of the 'Location' textbox in the store locator popup.
+        """
+        try:
+            element = self.driver.find_element(By.ID, self.locators['location_textbox']['value'])
+            return element.is_displayed()
+        except NoSuchElementException:
+            return False
+
+    def is_search_for_stores_button_present(self):
+        """
+        Verifies the presence of the 'Search for Stores' button in the popup.
+        """
+        try:
+            element = self.driver.find_element(By.XPATH, self.locators['search_for_stores_button']['value'])
+            return element.is_displayed()
+        except NoSuchElementException:
+            return False
