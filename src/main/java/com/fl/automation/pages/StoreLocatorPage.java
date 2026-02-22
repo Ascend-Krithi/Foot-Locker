5b2882d974f5718e2229bec3b9b1337006169685
gkiruthika1505 <gkiruthika1505@gmail.com>
2026-02-22 11:13:58 +0000

    Add automation scripts

---
+++ src/main/java/com/fl/automation/pages/StoreLocatorPage.java
@@ -0,0 +1,5 @@
+package com.fl.automation.pages;
+
+public class StoreLocatorPage {
+    // Placeholder for future store locator page actions
+}
---
+++ src/main/java/com/fl/automation/pages/StoreResultsPage.java
@@ -0,0 +1,5 @@
+package com.fl.automation.pages;
+
+public class StoreResultsPage {
+    // Placeholder for future store results page actions
+}
