f968785b05f5fc525e28b85118281f8cb641b5c3
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-02-22 19:24:55 +0530

    Deploying Page Objects

--- Scripts/TestScripts.py
+++ Scripts/TestScripts.py
@@ -2,54 +2,18 @@
 
 # TODO: Add test methods for testCaseId 2015, 2016, etc.
 
-import unittest
-from selenium import webdriver
-from pages.footlocker_home_page import FootLockerHomePage
-from pages.store_locator_page import StoreLocatorPage
+def test_case_2017():
+    """
+    TestCaseId: 2017
+    Description: Test Case - SCRUM-15408 TS-005 TC-001
+    """
+    # TODO: Implement test steps for TestCaseId 2017
+    pass
 
-class FootLockerStoreLocatorTests(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.home_page = FootLockerHomePage(self.driver)
-        self.store_locator = StoreLocatorPage(self.driver)
-
-    def tearDown(self):
-        self.driver.quit()
-
-    def test_store_locator_address_visible_2015(self):
-        """
-        TestCaseId: 2015
-        Steps:
-          1. Launch homepage
-          2. Click Find a Store, Select My Store
-          3. Enter 'Boston, MA', Search
-          4. Verify store address is visible
-        """
-        self.home_page.load()
-        self.home_page.click_find_a_store()
-        self.home_page.click_select_my_store()
-        self.store_locator.enter_location('Boston, MA')
-        self.store_locator.click_search()
-        result = self.store_locator.is_store_result_present('375 Washington Street, Boston, MA 02108')
-        self.assertTrue(result, "Store address '375 Washington Street, Boston, MA 02108' should be visible in results.")
-
-    def test_store_locator_set_my_store_2016(self):
-        """
-        TestCaseId: 2016
-        Steps:
-          1. Launch homepage
-          2. Click Find a Store, Select My Store
-          3. Enter 'Boston, MA', Search
-          4. Click Set My Store for address
-        """
-        self.home_page.load()
-        self.home_page.click_find_a_store()
-        self.home_page.click_select_my_store()
-        self.store_locator.enter_location('Boston, MA')
-        self.store_locator.click_search()
-        self.store_locator.click_set_my_store('375 Washington Street, Boston, MA 02108')
-        # Assume successful click means preferred store is set
-        # Optionally, add further assertions if UI feedback is available
-
-if __name__ == "__main__":
-    unittest.main()
+def test_case_2018():
+    """
+    TestCaseId: 2018
+    Description: Test Case - SCRUM-15408 TS-006 TC-001
+    """
+    # TODO: Implement test steps for TestCaseId 2018
+    pass
