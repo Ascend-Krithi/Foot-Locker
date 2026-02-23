dfb7db461b4db7ea75f8a60647a2e90b2f30cdb4
gkiruthika1505 <gkiruthika1505@gmail.com>
2026-02-23 07:08:03 +0000

    Add automation scripts

---
+++ src/test/java/com/fl/automation/tests/TS009_TC002_.java
@@ -0,0 +1,50 @@
+package com.fl.automation.tests;
+
+import com.fl.automation.core.ConfigReader;
+import com.fl.automation.core.DriverFactory;
+import com.fl.automation.pages.HomePage;
+import com.fl.automation.pages.StoreLocatorPage;
+import com.fl.automation.pages.StoreResultsPage;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Acceptance Criteria ID: AC2
+ * Test Scenario ID: SCRUM-15408 TS-009
+ * Test Case ID: TC-002
+ * Description: Leave location textbox empty, click Search for Stores if enabled and verify error message
+ */
+public class TS009_TC002_ {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        DriverFactory.initDriver();
+        driver = DriverFactory.getDriver();
+        driver.get(ConfigReader.get("base.url"));
+    }
+
+    @Test
+    public void testErrorMessageWhenLocationEmptyAndSearchClicked() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickFindAStore();
+
+        StoreLocatorPage locatorPage = new StoreLocatorPage(driver);
+        locatorPage.clickSelectMyStore();
+
+        StoreResultsPage resultsPage = new StoreResultsPage(driver);
+
+        if (resultsPage.isSearchButtonEnabled()) {
+            resultsPage.clickSearchButton();
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+}
