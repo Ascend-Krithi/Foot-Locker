7c977c1423b7188b921b15efbaf49b578067c8a7
gkiruthika1505 <gkiruthika1505@gmail.com>
2026-02-23 07:08:55 +0000

    Add automation scripts

---
+++ src/main/java/com/fl/automation/pages/HomePage.java
@@ -0,0 +1,29 @@
+package com.fl.automation.pages;
+
+import com.fl.automation.core.BrowserUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+
+    private By findAStoreLink = By.linkText("Find a Store");
+    private By findAStoreLinkCss = By.cssSelector("header a[href*='stores.footlocker.com']");
+    private By findAStoreLinkXpath = By.xpath("//header//a[contains(.,'Find a Store') or contains(.,'Store Locator')]");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickFindAStore() {
+        try {
+            BrowserUtils.click(driver, findAStoreLink);
+        } catch (Exception e1) {
+            try {
+                BrowserUtils.click(driver, findAStoreLinkCss);
+            } catch (Exception e2) {
+                BrowserUtils.click(driver, findAStoreLinkXpath);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/fl/automation/pages/StoreResultsPage.java
@@ -0,0 +1,113 @@
+package com.fl.automation.pages;
+
+import com.fl.automation.core.BrowserUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class StoreResultsPage {
+    private WebDriver driver;
+
+    private By searchInput1 = By.cssSelector("input[type='search']");
+    private By searchInput2 = By.cssSelector("input[name='q']");
+    private By searchInput3 = By.cssSelector("input[aria-label*='Search']");
+    private By searchInput4 = By.cssSelector("input[placeholder*='Search' i], input[placeholder*='City' i], input[placeholder*='ZIP' i]");
+    private By searchButton = By.xpath("//button[contains(.,'Search for Stores')]");
+    private By storeCards = By.cssSelector("[data-qa='location'], .c-location-card, .location, [class*='location-card']");
+    private By storeAddress = By.cssSelector("[data-qa='address'], .c-address, address, .address, [class*='address']");
+    private By setMyStoreButton = By.xpath(".//button[contains(.,'Select My Store') or contains(.,'Set My Store') or contains(.,'Make This My Store') or contains(.,'Set as My Store') or contains(.,'My Store')]");
+    private By noResultsMessage = By.xpath("//*[contains(.,'There are no locations in your search area')]");
+
+    public StoreResultsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isSearchInputDisplayed() {
+        try {
+            return BrowserUtils.isDisplayed(driver, searchInput1);
+        } catch (Exception e1) {
+            try {
+                return BrowserUtils.isDisplayed(driver, searchInput2);
+            } catch (Exception e2) {
+                try {
+                    return BrowserUtils.isDisplayed(driver, searchInput3);
+                } catch (Exception e3) {
+                    return BrowserUtils.isDisplayed(driver, searchInput4);
+                }
+            }
+        }
+    }
+
+    public boolean isSearchButtonDisplayed() {
+        return BrowserUtils.isDisplayed(driver, searchButton);
+    }
+
+    public void enterLocation(String location) {
+        try {
+            BrowserUtils.type(driver, searchInput1, location);
+        } catch (Exception e1) {
+            try {
+                BrowserUtils.type(driver, searchInput2, location);
+            } catch (Exception e2) {
+                try {
+                    BrowserUtils.type(driver, searchInput3, location);
+                } catch (Exception e3) {
+                    BrowserUtils.type(driver, searchInput4, location);
+                }
+            }
+        }
+    }
+
+    public void clickSearchButton() {
+        BrowserUtils.click(driver, searchButton);
+    }
+
+    public boolean areStoreResultsDisplayed() {
+        return BrowserUtils.isDisplayed(driver, storeCards);
+    }
+
+    public boolean isStoreAddressVisible(String address) {
+        List<WebElement> stores = driver.findElements(storeCards);
+        for (WebElement store : stores) {
+            try {
+                String storeAddressText = store.findElement(storeAddress).getText();
+                if (storeAddressText.contains(address)) {
+                    return true;
+                }
+            } catch (Exception e) {
+                continue;
+            }
+        }
+        return false;
+    }
+
+    public void clickSetMyStoreForAddress(String address) {
+        List<WebElement> stores = driver.findElements(storeCards);
+        for (WebElement store : stores) {
+            try {
+                String storeAddressText = store.findElement(storeAddress).getText();
+                if (storeAddressText.contains(address)) {
+                    store.findElement(setMyStoreButton).click();
+                    return;
+                }
+            } catch (Exception e) {
+                continue;
+            }
+        }
+    }
+
+    public boolean isNoResultsMessageDisplayed() {
+        return BrowserUtils.isDisplayed(driver, noResultsMessage);
+    }
+
+    public boolean isSearchButtonEnabled() {
+        try {
+            WebElement button = driver.findElement(searchButton);
+            return button.isEnabled();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
