486f3dc2225e9aad4a5f38233274024e09367ff7
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-03-05 19:46:39 +0530

    Update BaseTest.java

--- src/test/java/com/fl/automation/core/BaseTest.java
+++ src/test/java/com/fl/automation/core/BaseTest.java
@@ -1,26 +1,50 @@
-
 package com.fl.automation.core;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
+        // Ensure matching ChromeDriver
+        WebDriverManager.chromedriver().setup();
 
-        driver = DriverFactory.createDriver();
-        driver.get("https://www.footlocker.com");
+        ChromeOptions options = new ChromeOptions();
+
+        // Headless default in CI (can be off locally)
+        boolean headless = Boolean.parseBoolean(
+                System.getProperty("headless",
+                        System.getenv().getOrDefault("HEADLESS", "true"))
+        );
+        if (headless) {
+            options.addArguments("--headless=new");
+        }
+
+        // MUST-HAVE flags for Linux runners (GitHub Actions)
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--remote-allow-origins=*");
+
+        // If CI provided the Chrome binary path, honor it
+        String chromeBinary = System.getProperty("chromeBinary",
+                                System.getenv("CHROME_PATH"));
+        if (chromeBinary != null && !chromeBinary.isBlank()) {
+            options.setBinary(chromeBinary);
+        }
+
+        // Do NOT call maximize() in headless; rely on --window-size
+        driver = new ChromeDriver(options);
     }
 
-    @AfterMethod
-    public void teardown(){
-
-        if(driver != null){
-            driver.quit();
-        }
+    @AfterMethod(alwaysRun = true)
+    public void teardown() {
+        if (driver != null) driver.quit();
     }
 }
