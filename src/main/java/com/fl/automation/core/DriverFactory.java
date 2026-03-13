994ea2bf4b79d29766703e14dc73667926d60464
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-03-13 19:55:29 +0530

    Update DriverFactory.java

--- src/main/java/com/fl/automation/core/DriverFactory.java
+++ src/main/java/com/fl/automation/core/DriverFactory.java
@@ -1,38 +1,30 @@
 package com.fl.automation.core;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
-public class DriverFactory {
-    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+public class BaseTest {
 
-    public static WebDriver getDriver() {
-        if (driver.get() == null) {
-            // Automatically match ChromeDriver version to local Chrome
-            WebDriverManager.chromedriver().setup();
+    protected WebDriver driver;
 
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--incognito");
-            options.addArguments("--start-maximized");
-            options.addArguments("--disable-notifications");
-            options.addArguments("--remote-allow-origins=*"); // KB recommended
+    @BeforeMethod
+    public void setup() {
+        // Use existing getDriver() from DriverFactory
+        driver = DriverFactory.getDriver();
 
-            WebDriver webDriver = new ChromeDriver(options);
-            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
-            driver.set(webDriver);
-        }
-        return driver.get();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(90));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
+
+        driver.get("https://www.footlocker.com/");
     }
 
-    public static void quitDriver() {
-        if (driver.get() != null) {
-            driver.get().quit();
-            driver.remove();
+    @AfterMethod(alwaysRun = true)
+    public void teardown() {
+        if(driver != null){
+            DriverFactory.quitDriver();  // Use quitDriver method
         }
     }
 }
