49c973d76fe85bd77dfe588ec41f4f8264357fbf
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-03-13 19:38:48 +0530

    Update BaseTest.java

--- src/test/java/com/fl/automation/core/BaseTest.java
+++ src/test/java/com/fl/automation/core/BaseTest.java
@@ -2,35 +2,27 @@ package com.fl.automation.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
 public class BaseTest {
 
-    // ThreadLocal ensures each test thread gets its own WebDriver
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    protected static final String BASE_URL = "https://www.footlocker.com";
 
-    @BeforeMethod
-    public void setUp() {
-        WebDriver localDriver = new ChromeDriver();
-        localDriver.manage().window().maximize();
-        localDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.set(localDriver);
-        driver.get("https://www.footlocker.com"); // Base URL
+    public void initDriver() {
+        driver.set(new ChromeDriver());
+        driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    @AfterMethod
-    public void tearDown() {
+    public WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public void quitDriver() {
         if (driver.get() != null) {
             driver.get().quit();
             driver.remove();
         }
-    }
-
-    // Accessor for child classes
-    public WebDriver getDriver() {
-        return driver.get();
     }
 }
