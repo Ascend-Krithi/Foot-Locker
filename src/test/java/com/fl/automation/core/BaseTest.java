7e088ca0cd08cdb22b91ca23dc450f66aa19c06f
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-03-13 19:42:45 +0530

    Update BaseTest.java

--- src/test/java/com/fl/automation/core/BaseTest.java
+++ src/test/java/com/fl/automation/core/BaseTest.java
@@ -3,26 +3,32 @@ package com.fl.automation.core;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
-
 public class BaseTest {
 
-    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-    protected static final String BASE_URL = "https://www.footlocker.com";
+    // Keep driver private
+    private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    public void initDriver() {
-        driver.set(new ChromeDriver());
-        driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    // Initialize driver
+    public void setDriver(WebDriver webDriver) {
+        driver.set(webDriver);
     }
 
+    // Public getter to access driver safely
     public WebDriver getDriver() {
         return driver.get();
     }
 
+    // Quit driver
     public void quitDriver() {
         if (driver.get() != null) {
             driver.get().quit();
             driver.remove();
         }
+    }
+
+    // Example: start driver
+    public void initializeDriver() {
+        WebDriver webDriver = new ChromeDriver();
+        setDriver(webDriver);
     }
 }
