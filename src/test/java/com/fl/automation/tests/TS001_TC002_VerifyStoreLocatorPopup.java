359fa8e27181beb9defd0f27be1edb50ccff7ee7
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-03-17 11:07:37 +0530

    Update TS001_TC002_VerifyStoreLocatorPopup.java

--- src/test/java/com/fl/automation/tests/TS001_TC002_VerifyStoreLocatorPopup.java
+++ src/test/java/com/fl/automation/tests/TS001_TC002_VerifyStoreLocatorPopup.java
@@ -7,19 +7,30 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TS001_TC002_VerifyStoreLocatorPopup extends BaseTest {
+@Test(description = "TC4201 - TS-001 TC-002: Click Find a Store, click Select My Store, verify Location textbox and Search for Stores button")
+public void testVerifyStoreLocatorPopup() {
+    HomePage homePage = new HomePage(driver);
+    StoreLocatorHelper storeLocatorHelper = new StoreLocatorHelper(driver);
 
-    @Test(description = "TC4201 - TS-001 TC-002: Click Find a Store, click Select My Store, verify Location textbox and Search for Stores button")
-    public void testVerifyStoreLocatorPopup() {
-        HomePage homePage = new HomePage(driver);
-        StoreLocatorHelper storeLocatorHelper = new StoreLocatorHelper(driver);
-        
-        homePage.acceptCookiesIfPresent();
-        
-        homePage.clickFindStore();
-        
-        homePage.clickSelectMyStore();
-        
-        Assert.assertTrue(storeLocatorHelper.isLocationSearchInputDisplayed(), "Location search input should be displayed");
-        Assert.assertTrue(storeLocatorHelper.isSearchButtonDisplayed(), "Search for Stores button should be displayed");
-    }
+    // Handle cookies
+    homePage.acceptCookiesIfPresent();
+
+    // Open Store Locator flow
+    homePage.clickFindStore();
+    homePage.clickSelectMyStore();
+
+    // 🔥 CRITICAL FIX: wait for popup to fully load (CI-safe)
+    storeLocatorHelper.waitForStoreLocatorToLoad();
+
+    // Validations
+    Assert.assertTrue(
+            storeLocatorHelper.isLocationSearchInputDisplayed(),
+            "Location search input should be displayed"
+    );
+
+    Assert.assertTrue(
+            storeLocatorHelper.isSearchButtonDisplayed(),
+            "Search for Stores button should be displayed"
+    );
+}
 }
